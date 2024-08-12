3b0489741cef367d041cc4a328c317f9786398d9
groveer <groveer@outlook.com>
2024-08-12 09:11:07 +0800

    chore: replace recorder to bash file

---
+++ configs/.local/bin/recorder.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if pgrep wf-recorder; then
+    killall -s SIGINT wf-recorder
+    notify-send -t 3000 wf-recorder "record screen end"
+else
+    result=$(slurp)
+    file=/tmp/recording_$(date +%Y-%m-%d_%H:%M:%S).mp4
+    notify-send -t 5000 wf-recorder "begin record screen with geometry: $result, and will save to $file"
+    wf-recorder -g $result -f $file
+fi
