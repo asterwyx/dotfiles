808e9253299d864240a1f0bae5cb1d20ad69daca
groveer <groveer@outlook.com>
2024-08-17 13:14:02 +0800

    fix: recorder cannot select rect

--- configs/.local/bin/recorder.sh
+++ configs/.local/bin/recorder.sh
@@ -7,5 +7,5 @@ else
     result=$(slurp)
     file=/tmp/recording_$(date +%Y-%m-%d_%H:%M:%S).mp4
     notify-send -t 5000 wf-recorder "begin record screen with geometry: $result, and will save to $file"
-    wf-recorder -g $result -f $file
+    wf-recorder -g "$result" -f $file
 fi
